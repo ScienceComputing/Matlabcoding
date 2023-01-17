4ea3ad5661b978fe73d88b4dad8e06620592798c
Angela <90419301+lancome1@users.noreply.github.com>
2023-01-17 14:24:02 -0500

    Create Add_matrices_unb.m

---
+++ Add_matrices_unb.m
@@ -0,0 +1,47 @@
+% Copyright 2023 The University of Texas at Austin
+%
+% For licensing information see
+%                http://www.cs.utexas.edu/users/flame/license.html 
+%                                                                                 
+% Programmed by: Anni Liu
+
+% Interpret the function: http://edx-org-utaustinx.s3.amazonaws.com/UT501x/PictureFLAME/PictureFLAME.html
+
+function [ A_out ] = Add_matrices_unb( A, B )
+
+  [ AL, AR ] = FLA_Part_1x2( A, ...
+                               0, 'FLA_LEFT' );
+
+  [ BL, BR ] = FLA_Part_1x2( B, ...
+                               0, 'FLA_LEFT' );
+
+  while ( size( AL, 2 ) < size( A, 2 ) )
+
+    [ A0, a1, A2 ]= FLA_Repart_1x2_to_1x3( AL, AR, ...
+                                         1, 'FLA_RIGHT' );
+
+    [ B0, b1, B2 ]= FLA_Repart_1x2_to_1x3( BL, BR, ...
+                                         1, 'FLA_RIGHT' );
+
+    %------------------------------------------------------------%
+
+    a1 = a1 + b1;
+
+    %------------------------------------------------------------%
+
+    [ AL, AR ] = FLA_Cont_with_1x3_to_1x2( A0, a1, A2, ...
+                                           'FLA_LEFT' );
+
+    [ BL, BR ] = FLA_Cont_with_1x3_to_1x2( B0, b1, B2, ...
+                                           'FLA_LEFT' );
+
+  end
+
+  A_out = [ AL, AR ];
+
+return
+
+% Test the function:
+% A = randi([-2,2], 4, 3)
+% B = randi([-1,9], 4, 3)
+% Add_matrices_unb( A, B )
