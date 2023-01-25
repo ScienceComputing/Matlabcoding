dcc60cfb7d4f0792c2b59e79a8a040c8fa6f1e73
Phoenix <90419301+lancome1@users.noreply.github.com>
2023-01-25 17:04:50 -0500

    Update Mvmult_n_unb_var1B.m

--- Mvmult_n_unb_var1B.m
+++ Mvmult_n_unb_var1B.m
@@ -73,12 +73,12 @@ function [ y_out ] = Mvmult_n_unb_var1B( A, x, y )
 return
 
 % Test the function
-A = randi( [ -2, 2 ], 4, 4 );
-x = randi( [ -2, 2 ], 4, 1 );
-y = randi( [ -2, 2 ], 4, 1 );
+% A = randi( [ -2, 2 ], 4, 4 );
+% x = randi( [ -2, 2 ], 4, 1 );
+% y = randi( [ -2, 2 ], 4, 1 );
 
-if ( isequal( Mvmult_n_unb_var1B( A, x, y ), A * x + y ) )
-    disp( 'Mvmult_unb_var1B appears to be correct' )
-else
-    disp( 'Mvmult_unb_var1B has a problem' )
-end
+% if ( isequal( Mvmult_n_unb_var1B( A, x, y ), A * x + y ) )
+%     disp( 'Mvmult_unb_var1B appears to be correct' )
+% else
+%     disp( 'Mvmult_unb_var1B has a problem' )
+% end
