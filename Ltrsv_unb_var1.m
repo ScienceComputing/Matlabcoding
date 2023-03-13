c14f4526351c42c097746e658df1e462414128e4
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-13 08:35:20 -0400

    Create Ltrsv_unb_var1.m

---
+++ Ltrsv_unb_var1.m
@@ -0,0 +1,86 @@
+
+% Copyright 2023 The University of Texas at Austin
+%
+% For licensing information see
+%                http://www.cs.utexas.edu/users/flame/license.html 
+%                                                                                 
+% Programmed by: Anni Liu
+
+function [ b_out ] = Ltrsv_unb_var1( L, b )
+
+  [ LTL, LTR, ...
+    LBL, LBR ] = FLA_Part_2x2( L, ...
+                               0, 0, 'FLA_TL' );
+
+  [ bT, ...
+    bB ] = FLA_Part_2x1( b, ...
+                         0, 'FLA_TOP' );
+
+  while ( size( LTL, 1 ) < size( L, 1 ) )
+
+    [ L00,  l01,      L02,  ...
+      l10t, lambda11, l12t, ...
+      L20,  l21,      L22 ] = FLA_Repart_2x2_to_3x3( LTL, LTR, ...
+                                                     LBL, LBR, ...
+                                                     1, 1, 'FLA_BR' );
+
+    [ b0, ...
+      beta1, ...
+      b2 ] = FLA_Repart_2x1_to_3x1( bT, ...
+                                    bB, ...
+                                    1, 'FLA_BOTTOM' );
+
+    %------------------------------------------------------------%
+
+    b2 = laff_axpy( -beta1, l21, b2 );
+
+    %------------------------------------------------------------%
+
+    [ LTL, LTR, ...
+      LBL, LBR ] = FLA_Cont_with_3x3_to_2x2( L00,  l01,      L02,  ...
+                                             l10t, lambda11, l12t, ...
+                                             L20,  l21,      L22, ...
+                                             'FLA_TL' );
+
+    [ bT, ...
+      bB ] = FLA_Cont_with_3x1_to_2x1( b0, ...
+                                       beta1, ...
+                                       b2, ...
+                                       'FLA_TOP' );
+
+  end
+
+  b_out = [ bT
+            bB ];
+
+return
+
+% Test the function
+% Create a unit lower triangular matrixmatrix.  
+% (This matrix was carefully chosen so that only integers
+% are encountered during the process.)
+
+L = [
+     1     0     0     0
+    -1     1     0     0
+     2     1     1     0
+    -2    -1     1     1
+]
+
+% Create a right-hand side.  We are going to solve L x = b
+
+b = [
+     2
+     2
+    11
+    -3
+]
+
+% Solve L x = b
+
+x = Ltrsv_unb_var1( L, b )
+
+% Check that L x = b
+
+b - L * x
+
